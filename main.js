4f6bbeda080f7a4c526e075eb83cc958e9523855
gael-src <gaelfr85@gmail.com>
2020-05-12 08:12:06 +0200

    commit

--- main.js
+++ main.js
@@ -3,85 +3,85 @@ const employees = [
     name: "Vadim",
     email: "vadim@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e9/Maischberger_-_2019-11-13-9498-2.jpg",
-    hobbies: ["code", "bull", "bmw"],
+      "https://avatars2.githubusercontent.com/u/55791337?s=400&u=d2c9d80449cb05ab7ae3f6f655f7040e9aaf0015&v=4",
+    hobbies: ["code", "bmw", "work"],
   },
   {
     name: "Mariusz",
     email: "mariusz@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Jodie_Foster_C%C3%A9sars_2011_2_%28cropped%29.jpg",
+      "https://avatars2.githubusercontent.com/u/50579679?s=400&u=fed1dbed99f748599dacf9260ebdbc7d666df50f&v=4",
     hobbies: ["code", "painting", "design"],
   },
   {
     name: "Niels",
     email: "niels@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
-    hobbies: ["pot", "cocaine", "beer"],
+      "https://avatars1.githubusercontent.com/u/12913491?s=400&u=b311621271ea4c0a8e197eaeb5a5582c426ab2e8&v=4",
+    hobbies: ["code", "music", "master"],
   },
   {
     name: "Pawel",
     email: "pawel@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e9/Maischberger_-_2019-11-13-9498-2.jpg",
-    hobbies: ["code", "sleep", "cat"],
+      "https://avatars1.githubusercontent.com/u/50579605?s=400&u=5d1af461ffea1f90cea292cd60c498bcc6622c07&v=4",
+    hobbies: ["code", "cat", "done"],
   },
   {
     name: "Marco",
     email: "marco@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Jodie_Foster_C%C3%A9sars_2011_2_%28cropped%29.jpg",
-    hobbies: ["code", "n", "baby"],
+      "https://avatars3.githubusercontent.com/u/55787638?s=400&v=4",
+    hobbies: ["code", "twitch", "baby"],
   },
   {
     name: "Andreas",
     email: "andreas@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
+      "https://avatars3.githubusercontent.com/u/50579748?s=400&v=4",
     hobbies: ["code", "rap", "beer"],
   },
   {
     name: "Michael",
     email: "michael@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e9/Maischberger_-_2019-11-13-9498-2.jpg",
-    hobbies: ["code", "vegan", "pot"],
+      "https://www.saarbruecker-zeitung.de/imgs/03/1/3/8/7/2/3/4/tok_eea62df758d98f24cd14c38116378100/w900_h724_x450_y362_sl-stachurski_spotgun.jpg_1970-G9O56QFDE.1-ORG_CCIDIM-106-48112f8d0db68f1a.jpg",
+    hobbies: ["code", "vegan", "master"],
   },
   {
     name: "Melad",
     email: "melad@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Jodie_Foster_C%C3%A9sars_2011_2_%28cropped%29.jpg",
+      "https://avatars0.githubusercontent.com/u/55791371?s=400&v=4",
     hobbies: ["code", "math", "music"],
   },
   {
     name: "Kinan",
     email: "kinan@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
+      "https://avatars0.githubusercontent.com/u/57760356?s=400&v=4",
     hobbies: ["code", "sleep", "laugh"],
   },
   {
     name: "Victoria",
     email: "victoria@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/e9/Maischberger_-_2019-11-13-9498-2.jpg",
-    hobbies: ["code", "design", "brat"],
+      "https://avatars1.githubusercontent.com/u/54753921?s=400&u=3f9d8dcfb34642fee1108b80085ca7ff8b270009&v=4",
+    hobbies: ["code", "brat", "design"],
   },
   {
     name: "Majd",
     email: "majd@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Jodie_Foster_C%C3%A9sars_2011_2_%28cropped%29.jpg",
+      "https://avatars2.githubusercontent.com/u/40263435?s=400&v=4",
     hobbies: ["code", "game", "done"],
   },
   {
     name: "Gaël",
     email: "gael@apollo.io",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
-    hobbies: ["code", "design", "speak"],
+      "https://avatars2.githubusercontent.com/u/55788820?s=460&v=4",
+    hobbies: ["code", "ask", "design"],
   },
 ];
 
@@ -93,11 +93,19 @@ bodyEl.style.backgroundColor = "#fbd14b";
 bodyEl.style.textAlign = "center";
 bodyEl.style.fontFamily = "arial";
 
+bodyEl.style.minWidth = "100%";
+
+// let title = document.createElement("h1");
+// title.classList.add("title");
+// title.innerText = "Apollo 18";
+
 const mar = document.querySelector(".employees");
-mar.style.height = "80vh";
+mar.style.minHeight = "80vh";
+// mar.style.minWidth = "100%";
 mar.style.color = "#fbd14b";
 mar.style.fontWeight = "bold";
 mar.style.display = "flex";
+// mar.style.flexDirection = "column";
 mar.style.justifyContent = "space-evenly";
 mar.style.alignItems = "center";
 mar.style.textAlign = "center";
